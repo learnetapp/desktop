206834f9092a6abc3d7ae872e5ec2af7bde73dc1
decompile1 <decompilexyz@gmail.com>
2026-08-20 13:12:30 -0400

    hmmm

--- .github/build/appimage/get-dependencies.sh
+++ .github/build/appimage/get-dependencies.sh
@@ -2,10 +2,14 @@
 
 set -eu
 
+# Force pacman to never prompt for confirmation globally
+export PACMAN_CONFIRM=no  # (or check if the tool accepts flags)
+
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm libnss_nis nss-mdns nss pipewire
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+# Pass --noconfirm if the script/wrapper accepts arguments:
+get-debloated-pkgs --add-common --prefer-nano --noconfirm
