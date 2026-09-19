774672720dff0c034887c1fcf5cf490192d4bd4e
decompile1 <decompilexyz@gmail.com>
2026-09-19 09:47:45 -0400

    a

--- .github/build/appimage/extract-artifact.sh
+++ .github/build/appimage/extract-artifact.sh
@@ -6,8 +6,10 @@ echo "Extracting Artifact..."
 echo "---------------------------------------------------------------"
 
 mkdir -p ./AppDir/bin
-unzip /tmp/learnet/Learnet-linux-*.zip
-mv -v ./Learnet-linux-"$BUILD_ARCH"/* ./AppDir/bin
+tar -xzf /tmp/learnet/Learnet.tar.gz -C ./AppDir/bin
+
+echo "Contents of AppDir/bin:"
+ls -la ./AppDir/bin
 
 echo "Packaging as version $BUILD_VERSION"
 echo "$BUILD_VERSION" > ~/version
